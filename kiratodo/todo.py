60181cb4828f63389a129b389880bf9c0e179b70
KiraCodingBot <helloworld.stephanie@gmail.com>
2022-08-08 11:33:59 -0400

    Done

---
+++ kiratodo/todo.py
@@ -0,0 +1,97 @@
+"""This module provides the kira To-Do model-controller."""
+
+import configparser
+import json
+from pathlib import Path
+from typing import Any, Dict, List, NamedTuple
+
+from kiratodo import DB_READ_ERROR, DB_WRITE_ERROR, JSON_ERROR, SUCCESS,ID_ERROR
+
+
+
+from kiratodo.database import DatabaseHandler
+
+class CurrentTodo(NamedTuple):
+    todo: Dict[str, Any]
+    error: int
+
+class DBResponse(NamedTuple):
+    todo_list: List[Dict[str, Any]]
+    error: int
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self._db_path = db_path
+
+    def read_todos(self) -> DBResponse:
+        try:
+            with self._db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError:  # Catch wrong JSON format
+                    return DBResponse([], JSON_ERROR)
+        except OSError:  # Catch file IO problems
+            return DBResponse([], DB_READ_ERROR)
+
+    def write_todos(self, todo_list: List[Dict[str, Any]]) -> DBResponse:
+        try:
+            with self._db_path.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, SUCCESS)
+        except OSError:  # Catch file IO problems
+            return DBResponse(todo_list, DB_WRITE_ERROR)
+
+class Todoer:
+    def __init__(self, db_path: Path) -> None:
+        self._db_handler = DatabaseHandler(db_path)
+
+    def add(self, description: List[str], priority: int = 2) -> CurrentTodo:
+        """Add a new to-do to the database."""
+        description_text = " ".join(description)
+        if not description_text.endswith("."):
+            description_text += "."
+        todo = {
+            "Description": description_text,
+            "Priority": priority,
+            "Done": False,
+        }
+        read = self._db_handler.read_todos()
+        if read.error == DB_READ_ERROR:
+            return CurrentTodo(todo, read.error)
+        read.todo_list.append(todo)
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
+    def get_todo_list(self) -> List[Dict[str, Any]]:
+        """Return the current to-do list."""
+        read = self._db_handler.read_todos()
+        return read.todo_list
+    def set_done(self, todo_id: int) -> CurrentTodo:
+        """Set a to-do as done."""
+        read = self._db_handler.read_todos()
+        if read.error:
+            return CurrentTodo({}, read.error)
+        try:
+            todo = read.todo_list[todo_id - 1]
+        except IndexError:
+            return CurrentTodo({}, ID_ERROR)
+        todo["Done"] = True
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
+    
+    
+    def remove(self, todo_id: int) -> CurrentTodo:
+        """Remove a to-do from the database using its id or index."""
+        read = self._db_handler.read_todos()
+        if read.error:
+            return CurrentTodo({}, read.error)
+        try:
+            todo = read.todo_list.pop(todo_id - 1)
+        except IndexError:
+            return CurrentTodo({}, ID_ERROR)
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
+
+    def remove_all(self) -> CurrentTodo:
+        """Remove all to-dos from the database."""
+        write = self._db_handler.write_todos([])
+        return CurrentTodo({}, write.error)
